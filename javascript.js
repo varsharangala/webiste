b4a5407ec9dce7cd38c02d694b17b8858cf268d4
varsharangala <82026555+varsharangala@users.noreply.github.com>
2021-12-04 19:34:54 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 10) {
+		$(".navbar").addClass("nav-scroll");
+	} else{
+		$(".navbar").removeClass("nav-scroll");
+	}
+});
